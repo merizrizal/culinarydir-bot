fb984f6d9d9266b8bf0db4a3edd7bbdc8bbe7724
Mei Rizal <meriz.rizal@gmail.com>
2019-06-21 23:56:15 +0700

    Update

--- controllers/BotHookController.php
+++ controllers/BotHookController.php
@@ -41,13 +41,15 @@ class BotHookController extends \yii\rest\Controller
 
     public function actionIndex() {
 
+        $post = \Yii::$app->request->post();
+
         $client = new Client();
         $response = $client->createRequest()
             ->setMethod('POST')
             ->setUrl('https://api.telegram.org/bot557170275:AAGwiZs-bNX_tiOHzqH_4wonYL5hwaoQtSg/sendMessage')
             ->setData([
                 'chat_id' => '899157364',
-                'text' => 'Hai kakak'
+                'text' => $post
             ])
             ->send();
 
